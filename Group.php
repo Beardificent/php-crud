f0257fb76763c26c43c90dd86959d546e71d288f
HENDRIK <hendrik@KALI.home>
2020-11-02 11:50:28 +0100

    added Group class and Model loader

---
+++ Group.php
@@ -0,0 +1,16 @@
+<?php
+
+Class Group
+{
+    private string $name;
+    private string $location;
+    private int $teacherId;
+    private array $students;
+
+    public function __construct()
+    {
+
+    }
+
+
+}
