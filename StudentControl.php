a68603a9dbd07e2db3bc7bbcf5a19d6a73bb9d50
Beardificent <cis.magito@gmail.com>
2020-11-02 13:24:05 +0100

    working on function addStudent (insert into db)

--- StudentControl.php
+++ StudentControl.php
@@ -4,4 +4,16 @@
 class StudentControl
 {
 
+
+    public function addStudent()
+    {
+        if (isset($_POST['submit'])){
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $class = $_POST['group'];
+            $teacher = $_POST['coach'];
+
+            $pdo->prepare("INSERT INTO student (name, email, class, teacher) VALUES ($name', '$email', '$class', '$teacher')")
+        }
+    }
 }
