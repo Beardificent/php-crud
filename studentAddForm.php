7ea34ed8f34cb7a350f4b9967a9600b8ae928248
Beardificent <cis.magito@gmail.com>
2020-11-02 12:21:58 +0100

    added form for student

---
+++ studentAddForm.php
@@ -0,0 +1,20 @@
+
+<form action="" method="post">
+    <p>
+        <label for="name">name:</label><br/>
+        <input type="text" name="name" id="name">
+    </p>
+    <p>
+        <label for="email">email:</label><br/>
+        <input type="text" name="email" id="email">
+    </p>
+    <p>
+        <label for="group">group:</label><br/>
+        <input type="text" name="group" id="group">
+    </p>
+    <p>
+        <label for="coach">coach:</label><br/>
+        <input type="text" name="coach" id="coach">
+    </p>
+    <input type="submit" value="Submit">
+</form>
