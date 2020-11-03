dce3eb89b344fc689f1565a0027ce497d94a7a0a
Beardificent <cis.magito@gmail.com>
2020-11-03 15:42:30 +0100

    wrapped forms around buttons

--- studentOverview.php
+++ studentOverview.php
@@ -9,8 +9,8 @@
            <p class="card-text">' . $student['email'] . '</p>
             </div>
             </div><br/>';
-    echo '<input type="submit" name="edit_student" value="edit student">';
-    echo '<input type="submit" name="user" value="' . $student['id'] .'" >';
+    echo '<form action="" method="post"></form><input type="submit" name="edit_student" value="edit student"></form>';
+    echo '<form action="" method="post"><input type="submit" name="delete_student" value="delete"></form>';
     //tried creating a link as delete function, also adding a 'send = del' to be able to look in url if send === to del a
      //and only if send = del, will it delete. but it's not working as I thought it would. But think the href is the problem.
     //echo '<a href="StudentOverviewControl.php?id=' . $student['id'] . '?' . '&send=del">Delete</a>';
