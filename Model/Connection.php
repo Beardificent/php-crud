b7f6ae6c6935250c777b03b5b75cb3be4c746f52
HENDRIK <hendrik@KALI.home>
2020-11-03 12:29:43 +0100

    has a getGroup and getStudentsInGroup function now

--- Model/Connection.php
+++ Model/Connection.php
@@ -74,4 +74,34 @@ class Connection
 
     }
 
+    public function addGroup($name, $location, $teacher)
+    {
+
+        $pdo = $this->openConnection();
+        $sql ="INSERT INTO Class (name, Location) VALUES (:name, :location)";
+        $result = $pdo->prepare($sql);
+        $result->bindValue(':name', $name);
+        $result->bindValue(':location', $location);
+        $result->bindValue(':teacher', $teacher);
+        $result->execute();
+
+    }
+    public function getGroup($id){
+        $pdo = $this->openConnection();
+        $sql ="SELECT * FROM Class  WHERE id = :id";
+        $result = $pdo->prepare($sql);
+        $result->bindValue(':name', $id);
+        $result->execute();
+        return $result->fetch();
+    }
+
+    public function getStudentsInGroup($id){
+        $pdo = $this->openConnection();
+        $sql ="SELECT * FROM Students  WHERE class_id = :id";//class_id is a placeholder for now
+        $result = $pdo->prepare($sql);
+        $result->bindValue(':id', $id);
+        $result->execute();
+        return $result->fetchAll();
+    }
+
 }
