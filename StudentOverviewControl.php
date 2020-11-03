750cece827260ce63c86a181d07f684256ce9b2f
Beardificent <cis.magito@gmail.com>
2020-11-03 15:48:01 +0100

    i think i'm so close to cracking this, it's driving me insane, wish i had an angel looking over my shoulder

--- StudentOverviewControl.php
+++ StudentOverviewControl.php
@@ -17,9 +17,17 @@ class StudentOverviewControl
     //Function doesn't work. this where i'd ask $GET to see if send = del in url to delete but it's not picking up the required page after.
     public function deleteRow()
     {
-        if ($_GET['send'] === 'del') {
+       /* if ($_GET['send'] === 'del') {
          echo "student has been deleted";
         }
+       */if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if($_POST['delete_student']){
+            echo "the student has been deleted";
+            $id = $_GET['id'];
+            $connection = new Connection();
+            $connection->deleteStudent($id);
+        }
+    }
 
     }
 
