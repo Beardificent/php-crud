2139d4b5435afcb062165ad47809a21f40feb327
Beardificent <cis.magito@gmail.com>
2020-11-03 16:45:01 +0100

    form action for delete button is set to page studentoverview

--- studentOverview.php
+++ studentOverview.php
@@ -12,7 +12,7 @@
     echo '<form action="" method="post"></form><input type="submit" name="edit_student" value="edit student"></form>';
 
     //By changing the value to $studentID, the button now says the id ofcourse. Do I need a hidden input? Nope, it works the numbers stay for now.
-     echo '<form action="" method="post"><input type="submit" name="delete_student" value="' . $student['id'] . '">DELETE</form>';
+     echo '<form action="?page=studentOverview" method="post"><input type="submit" name="delete_student" value="' . $student['id'] . '">DELETE</form>';
 
     //tried creating a link as delete function, also adding a 'send = del' to be able to look in url if send === to del a
      //and only if send = del, will it delete. but it's not working as I thought it would. But think the href is the problem.
