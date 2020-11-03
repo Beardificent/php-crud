129aa8bf6fc10b4636222bb79dec21e310166682
HENDRIK <hendrik@KALI.home>
2020-11-03 16:52:28 +0100

    controller logic

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -2,9 +2,36 @@
 
 Class GroupController{
 
-    function showAllStudents(){
-    $connection = new Connection();
-    $connection->getGroups();
-    include 'View/groupOverview.php';
+    public function __construct()
+    {
+        if($_POST["groupSubmit"] == "Delete")
+        {
+            //do a delete query
+        }
+        elseif(isset($_POST["id"]))
+        {
+            //do an edit query
+        }
+        elseif(isset($_POST["name"]))
+        {
+            //do an add query
+        }
+        elseif($_POST["groupSubmit"] == "Overview"))
+        {
+            //do an overview query
+            // include overview page
+        }
+        elseif($_POST["groupSubmit"] == "Edit"))
+        {
+            // include edit page
+        }
+        elseif($_POST["groupSubmit"] == "Add"))
+        {
+            // include add page
+        }
+
+
     }
+
+
 }
--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
 
+if(isset($_POST["StudentSubmit"]))
+{
+
+}
+elseif (isset($_POST["TeacherSubmit"]))
+{
+
+}
+elseif (isset($_POST["GroupSubmit"]))
+{
+    $app = new GroupController();
+}
