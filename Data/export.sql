24a9da0827729f69d1da729b403148a9288595ba
HENDRIK <hendrik@KALI.home>
2020-11-04 10:29:57 +0100

    new database

--- Data/export.sql
+++ Data/export.sql
@@ -1,100 +1,22 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
---
--- Host: 127.0.0.1    Database: Class Data
--- ------------------------------------------------------
--- Server version	8.0.22-0ubuntu0.20.04.2
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Class`
---
-
-DROP TABLE IF EXISTS `Class`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Class` (
-  `name` varchar(255) DEFAULT NULL,
-  `Location` varchar(255) DEFAULT NULL,
-  `assigned_teacher` varchar(255) DEFAULT NULL,
-  `list _assigned _students` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Class`
---
-
-LOCK TABLES `Class` WRITE;
-/*!40000 ALTER TABLE `Class` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Class` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Students`
---
-
-DROP TABLE IF EXISTS `Students`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Students` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `assigned_teacher` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Students`
---
-
-LOCK TABLES `Students` WRITE;
-/*!40000 ALTER TABLE `Students` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Students` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Teacher`
---
-
-DROP TABLE IF EXISTS `Teacher`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Teacher` (
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `list _of_students` int DEFAULT NULL,
-  `class_number` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Teacher`
---
-
-LOCK TABLES `Teacher` WRITE;
-/*!40000 ALTER TABLE `Teacher` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Teacher` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2020-11-02 14:05:02
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `name` varchar(100) NOT NULL,
+                           `email` varchar(100) NOT NULL,
+                           `class` int(11) NOT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `teacher`;
+CREATE TABLE `teacher` (
+                           `id` int(11) NOT NULL AUTO_INCREMENT,
+                           `name` varchar(100) NOT NULL,
+                           `class` int(11) NOT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `group`;
+CREATE TABLE `group` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `name` varchar(100) NOT NULL,
+                         `location` varchar(100) NOT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
