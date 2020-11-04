f8cb643f16f00b20994aec93f51093a54127056c
Beardificent <cis.magito@gmail.com>
2020-11-04 12:14:58 +0100

    copied the add student form and renamed it editstudentform to edit data

---
+++ View/editStudentForm.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Edit student</title>
+</head>
+<body>
+
+<form action="" method="post">
+    <p>
+        <label for="name">name:</label><br/>
+        <input type="text" name="name" placeholder="enter your name">
+    </p>
+    <p>
+        <label for="email">email:</label><br/>
+        <input type="text" name="email" placeholder=" your email">
+    </p>
+
+    <p>
+        <label for="group">class:</label><br/>
+        <input type="text" name="group" placeholder="the class you are in">
+    </p>
+
+    <p>
+        <label for="coach">coach:</label><br/>
+        <input type="text" name="coach" placeholder="who's giving the class?">
+    </p>
+
+    <input type="submit" name="submit" value="submit">
+</form>
+
+
+
+</body>
+</html>
